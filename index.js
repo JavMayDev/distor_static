2a681339208823f1cc6cc6147f442a194a3b492b
javi_mac <javimayorque@gmail.com>
2021-03-03 15:27:36 -0600

    basic model

--- index.js
+++ index.js
@@ -1,67 +1,76 @@
 var state = {
     prev: { bottom: '200%' },
     current: { bottom: '50%' },
-    next: { bottom: '-100%' }
-}
+    next: { bottom: '-100%' },
+};
 
-var sections = Array.from(document.getElementsByClassName('section'))
+var sections = Array.from(document.getElementsByClassName('section'));
 
 // set up material
-var material = new Blotter.RollingDistortMaterial()
-material.uniforms.uSpeed.value = 0.2
-material.uniforms.uRotation.value = 90
-material.uniforms.uNoiseDistortAmplitude.value = 0.5
-material.uniforms.uSineDistortAmplitude.value = 0.5
+var material = new Blotter.RollingDistortMaterial();
+material.uniforms.uSpeed.value = 0.2;
+material.uniforms.uRotation.value = 90;
+material.uniforms.uNoiseDistortAmplitude.value = 1;
+material.uniforms.uSineDistortAmplitude.value = 1;
 
-animateMaterial({ uNoiseDistortAmplitude: 0, uSineDistortAmplitude: 0 }, 500)
+animateMaterial(
+    { uNoiseDistortAmplitude: 0, uSineDistortAmplitude: 0 },
+    1000,
+    0
+);
 
 // set first as current
-Array.from(sections[0].getElementsByTagName('*')).forEach(child =>
-    Object.assign(child.style, JSON.parse(child.getAttribute('current')))
-)
+Array.from(sections[0].getElementsByTagName('*')).forEach(function (child) {
+    Object.assign(child.style, JSON.parse(child.getAttribute('current')));
+});
 
 // and the rest as next
-for (let i = 1; i < sections.length; i++)
-    Array.from(sections[i].getElementsByTagName('*')).forEach(child => {
-	console.log( 'whats up' )
-        Object.assign(child.style, state.next)
-    })
+for (var i = 1; i < sections.length; i++)
+    Array.from(sections[i].getElementsByTagName('*')).forEach(function (child) {
+        console.log('whats up');
+        Object.assign(child.style, state.next);
+    });
 
 sections.forEach(function (section) {
     Array.from(section.getElementsByTagName('*'))
         .filter(function (sectionChild) {
-            if (sectionChild.classList.contains('distor')) return sectionChild
+            if (sectionChild.classList.contains('distor')) return sectionChild;
         })
         .forEach(function (distor) {
-            var text = new Blotter.Text(distor.innerHTML, distor.dataset)
-            distor.innerHTML = ''
+            var text = new Blotter.Text(distor.innerHTML, distor.dataset);
+            distor.innerHTML = '';
             var blotter = new Blotter(material, {
                 texts: text,
-            })
-            var scope = blotter.forText(text)
-            scope.appendTo(distor)
+            });
+            var scope = blotter.forText(text);
+            scope.appendTo(distor);
 
-	    // add background color to the canvas
-	    if(distor.getAttribute('background-color')) 
-		distor.getElementsByTagName('canvas').item(0).style.backgroundColor = 
-		    distor.getAttribute('background-color')
-        })
-})
+            // add background color to the canvas
+            if (distor.getAttribute('background-color'))
+                distor
+                    .getElementsByTagName('canvas')
+                    .item(0).style.backgroundColor = distor.getAttribute(
+                    'background-color'
+                );
+        });
+});
 
 // set menu to select section
-const menu = document.getElementById('menu')
-sections.forEach((_, i) => {
-    const li = document.createElement('li')
-    li.innerHTML = i + 1
-    li.onclick = _ => setCurrent(i)
-    menu.appendChild(li)
-})
+var menu = document.getElementById('menu');
+sections.forEach(function (_, i) {
+    var li = document.createElement('li');
+    li.innerHTML = i + 1;
+    li.onclick = function (_) {
+        setCurrent(i);
+    };
+    menu.appendChild(li);
+});
 
 // set 'scrolling' (isn't really scrolling)
-window.onwheel = event => {
+window.onwheel = function (event) {
     // if go down and current isn't last
     if (event.deltaY >= 0 && current < sections.length - 1)
-        setCurrent(current + 1)
+        setCurrent(current + 1);
     // if go up and current isn't first
-    if (event.deltaY <= 0 && current > 0) setCurrent(current - 1)
-}
+    if (event.deltaY <= 0 && current > 0) setCurrent(current - 1);
+};
--- setCurrent.js
+++ setCurrent.js
@@ -1,100 +1,107 @@
-var scrollTime = 2000
-var scrollLock = false
-var current = 0 // current section index
- 
-function animateMaterial (materialSettings, duration = scrollTime /2) {
-    Object.keys(materialSettings).forEach(function(uniform){
-	anime({
-	    targets: material.uniforms[uniform],
-	    value: materialSettings[uniform],
-	    duration: duration,
-	    easing: 'linear'
-	})
-    })
+var scrollTime = 2000;
+var scrollLock = false;
+var current = 0; // current section index
+
+function animateMaterial(materialSettings, duration, delay) {
+    Object.keys(materialSettings).forEach(function (uniform) {
+        anime({
+            targets: material.uniforms[uniform],
+            value: materialSettings[uniform],
+            duration: duration,
+            easing: 'linear',
+	    delay: delay
+       });
+    });
 }
 
 function setCurrent(targetIndex) {
     // execute only once at time
-    if (scrollLock || targetIndex == current) return
-	scrollLock = true
+    if (scrollLock || targetIndex == current) return;
+    scrollLock = true;
 
-    animateMaterial({ uNoiseDistortAmplitude: 1, uSineDistortAmplitude: 1 }, 0)
+    animateMaterial({ uNoiseDistortAmplitude: 1, uSineDistortAmplitude: 1 }, 0, 0);
 
-    var incr, frameStatus
+    var incr, frameStatus;
     if (targetIndex > current) {
-        incr = 1
-        frameStatus = state.prev
+        incr = 1;
+        frameStatus = state.prev;
     } else {
-        incr = -1
-        frameStatus = state.next
+        incr = -1;
+        frameStatus = state.next;
     }
-    var duration = scrollTime / 3 / Math.abs(targetIndex - current)
-    var easing = 'linear'
-    var complete = frame // recursion
-    var isTarget = false
+    var duration = scrollTime / 3 / Math.abs(targetIndex - current);
+    var easing = 'linear';
+    var complete = frame; // recursion
+    var isTarget = false;
 
     function frame() {
-	// idk why but it has to be broken like this
-	// instead of just set complete as null
-	if(targetIndex == current) return
+        // idk why but it has to be broken like this
+        // instead of just set complete as null
+        if (targetIndex == current) return;
 
         // move current section
-        animateChildren(sections[current], {
-	    ...frameStatus,
-            easing,
-            duration,
-        }, isTarget)
+        animateChildren(
+            sections[current],
+            Object.assign({ easing: easing, duration: duration }, frameStatus),
+            isTarget
+        );
 
         // increase or decrease current index
-        current += incr
+        current += incr;
         // if done
         if (current == targetIndex) {
             // smoother easing
-            easing = 'easeOutQuart'
-            duration = scrollTime * 0.66
-            isTarget = true
-	    complete = null // break recursion
+            easing = 'easeOutQuart';
+            duration = scrollTime * 0.66;
+            isTarget = true;
+            complete = null; // break recursion
         }
 
-	// set 'next' section as current
+        // set 'next' section as current
         animateChildren(
             sections[current],
-            {
-                ...state.current,
-                easing,
-                duration,
-                complete,
-            },
-            isTarget,
-        )
+            Object.assign(
+                { easing: easing, duration: duration, complete: complete },
+                state.current
+            ),
+            isTarget
+        );
     }
 
-    frame()
+    frame();
 }
 
 function animateChildren(parent, animeSettings, isTarget) {
-    const children = Array.from(parent.getElementsByTagName('*'))
-	// exclude blotter canvas
-	.filter(function(child){
-	    if(!child.classList.contains('b-canvas')) return child
-	})
+    var children = Array.from(parent.getElementsByTagName('*'))
+        // exclude blotter canvas
+        .filter(function (child) {
+            if (!child.classList.contains('b-canvas')) return child;
+        });
 
-    children.forEach((child, i) => {
-	var customSettings = animeSettings
-	if(isTarget) {
-	    Object.assign(customSettings, JSON.parse(child.getAttribute('current')))
-	    animateMaterial({ uNoiseDistortAmplitude: 0, uSineDistortAmplitude: 0 })
-	    if (i == children.length - 1) // the last animated element
-		animeSettings.complete = () => scrollLock = false
-	}
+    children.forEach(function (child, i) {
+        var customSettings = animeSettings;
+        if (isTarget) {
+            Object.assign(
+                customSettings,
+                JSON.parse(child.getAttribute('current'))
+            );
 
-        anime({
-            targets: child,
-	    ...customSettings,
-            delay:
-		child.classList.contains('delay')
-                    ? child.getAttribute('delaytime')
-                    : 0,
-        })
-    })
+            if (child.classList.contains('delay'))
+                customSettings.delay = child.getAttribute('delaytime');
+            if (i == children.length - 1)
+                // the last animated element
+                animeSettings.complete = function () {
+                    scrollLock = false;
+                };
+            animateMaterial(
+                {
+                    uNoiseDistortAmplitude: 0,
+                    uSineDistortAmplitude: 0,
+                },
+                scrollTime / 2, customSettings.delay ? customSettings.delay : 0
+            );
+        }
+
+        anime(Object.assign({ targets: child }, customSettings));
+    });
 }
